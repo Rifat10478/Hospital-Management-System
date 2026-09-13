e24767c7fa7fa6a4fe2481d8a9b55cae0e7b6764
Islamul Haque <islamul0123@gmail.com>
2026-09-13 16:38:56 +0600

    Update PrescriptionEdit.jsx

--- frontend/src/pages/prescriptions/PrescriptionEdit.jsx
+++ frontend/src/pages/prescriptions/PrescriptionEdit.jsx
@@ -46,7 +46,7 @@ function PrescriptionEdit() {
 
 
             const response = await fetch(
-                `http://127.0.0.1:8000/api/hospital/prescriptions/${id}/`,
+                `https://hospital-management-system-zjkw.onrender.com/api/hospital/prescriptions/${id}/`,
                 {
                     headers: {
                         Authorization:
@@ -115,7 +115,7 @@ function PrescriptionEdit() {
 
 
             const response = await fetch(
-                `http://127.0.0.1:8000/api/hospital/prescriptions/${id}/`,
+                `https://hospital-management-system-zjkw.onrender.com/api/hospital/prescriptions/${id}/`,
                 {
                     method: "PUT",
 
